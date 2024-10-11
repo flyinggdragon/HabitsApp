3765a40740092656a25f62f750a2d2b849ba95e2
gairin <marco14963@gmail.com>
2024-10-10 23:47:34 -0300

    Início de desenvolvimento

---
+++ src/app/habits-list/habits-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'habits-list',
+  templateUrl: './habits-list.component.html',
+  styleUrls: ['./habits-list.component.css']
+})
+export class HabitsListComponent {
+
+}
